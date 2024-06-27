972ad117e63457258eb494a11295c4cb332eb939
timhams <timtam54@hotmail.com>
2024-06-28 09:15:31 +1000

    deploy

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,13 +4,11 @@ import styles from "../page.module.css";
 import { io } from "socket.io-client";
 import { Toaster, toast } from "sonner";
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-
+import ChatPage from "@/components/page";
 export default function Admin() 
 {
     var socket: any;
     socket = io("https://aschat.azurewebsites.net");//http://localhost:8080");
-  
-
     useEffect(() => {
         //socket.on("connect",()=>{
            // console.log('connected to 8080');
@@ -48,7 +46,8 @@ export default function Admin()
               
             >
               <h3>
-                <a href={"./adminchat?client="+chat} target="_other"><u>{chat}</u></a>
+                <h1><u>{chat}</u></h1>
+                <ChatPage socket={socket} client={chat} />
               </h3>
               <Toaster />
             </div>
