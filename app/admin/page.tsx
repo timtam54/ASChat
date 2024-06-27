4d68a3c4450e7db90d66715d59a1508b3afc0dfd
timhams <timtam54@hotmail.com>
2024-06-28 08:05:30 +1000

    first commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,60 @@
+"use client"
+import { useEffect, useState } from "react";
+import styles from "../page.module.css";
+import { io } from "socket.io-client";
+import { Toaster, toast } from "sonner";
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+
+export default function Admin() 
+{
+    var socket: any;
+    socket = io("https://aschat.azurewebsites.net");//http://localhost:8080");
+  
+
+    useEffect(() => {
+        //socket.on("connect",()=>{
+           // console.log('connected to 8080');
+            socket.on('room',async (data:string)  => {
+                //alert('room:'+data);    
+                if (!chats.includes(data))
+                    {
+                      new Audio('./sound/message.mp3').play();
+                setChats([...chats,data]);
+                toast(data, {
+                  position: 'bottom-left',
+                action: {
+                label: 'More',
+                onClick: () => alert(data),
+                },
+                description: 'chat with '+data +'`',
+                duration: 5000,
+                icon: <NotificationsNoneIcon />,
+                });
+                    }
+             });
+     
+          //  });
+      }, [socket]);
+
+      const [chats,setChats]=useState<string[]>([]);
+
+    return (
+        <div>
+            All client Chats - click conversation to join
+            <div>
+          {chats.map(( chat , key) => (
+            <div
+              key={key}
+              
+            >
+              <h3>
+                <a href={"./adminchat?client="+chat} target="_other"><u>{chat}</u></a>
+              </h3>
+              <Toaster />
+            </div>
+          ))}
+        </div>
+        </div>
+    )
+}
+//https://stackademic.com/blog/building-a-real-time-chat-app-with-next-js-socket-io-and-typescript
---
+++ app/page.tsx
@@ -0,0 +1,135 @@
+"use client"
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import { io } from "socket.io-client";
+
+
+interface IMsgDataTypes {
+    roomId: String | number;
+    user: String;
+    msg: String;
+    time: String;
+  }
+export default function ClientChat() 
+{
+
+    var socket: any;
+  socket = io("https://aschat.azurewebsites.net");//"http://localhost:8080");//https://aschat.azurewebsites.net
+
+
+    const sendData = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        
+        if (currentMsg !== "") {
+          const msgData: IMsgDataTypes = {
+            roomId : userName,
+            user: userName,
+            msg: currentMsg,
+            time:
+              new Date(Date.now()).getHours() +
+              ":" +
+              new Date(Date.now()).getMinutes(),
+          };
+          await socket.emit("send_msg", msgData);
+          setCurrentMsg("");
+        }
+      };
+
+    const [userName, setUserName] = useState("");
+    const [currentMsg, setCurrentMsg] = useState("");
+    const [chat, setChat] = useState<IMsgDataTypes[]>([]);
+  
+
+    useEffect(() => {
+        socket.on("receive_msg", (data: IMsgDataTypes) => {
+          new Audio('./sound/message.mp3').play();
+          setChat((pre) => [...pre, data]);
+        });
+      }, [socket]);
+      const [showSpinner, setShowSpinner] = useState(false);
+      const handleJoin = () => {
+        if (userName !== "" ) {
+          console.log(userName, "userName");
+          socket.emit("join_room", userName);
+          setShowSpinner(true);
+          setTimeout(() => {
+            setNameLock(true);
+            setShowSpinner(false);
+          }, 4000);
+          
+
+          // You can remove this setTimeout and add your own logic
+         
+        } else {
+          alert("Please fill in Username and Room Id");
+        }
+      };
+
+      const [namelock,setNameLock] = useState(false);
+
+    return (
+      <div className={styles.chat_div}>
+      <div className={styles.chat_border}>
+           <h3>Client chat to Admin</h3>
+        <form onSubmit={(e) => sendData(e)}>
+          <div style={{ marginBottom: "1rem" ,display:'flex'}}>
+            <b>Your Name</b>
+            {namelock?userName:
+            <>
+            <input
+          className={styles.main_input}
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+         <button onClick={handleJoin} >
+         {!showSpinner ? (
+            "Start"
+          ) : (
+            <div className={styles.loading_spinner}></div>
+          )}
+           </button>
+        </>
+      }
+       </div>
+        {!namelock?<></>:
+        <>
+        <div style={{display:'flex'}}>
+            <b>Message</b>
+            <input
+              className={styles.chat_input}
+              type="text"
+              value={currentMsg}
+              placeholder="Type your message.."
+              onChange={(e) => setCurrentMsg(e.target.value)}
+            />
+        </div>
+        <button className={styles.chat_button}>Send</button>
+        </>}
+        </form>
+        <div>
+          {chat.map(({ user, msg, time }, key) => (
+            <div
+              key={key}
+              className={
+                user == userName
+                  ? styles.chatProfileRight
+                  : styles.chatProfileLeft
+              }>
+              <span className={styles.chatProfileSpan}
+                style={{ textAlign: user == userName ? "right" : "left" }}>
+                {user.charAt(0)}
+              </span>
+              <h3 style={{ textAlign: user == userName ? "right" : "left" }}>
+                {msg}
+              </h3>
+            </div>
+          ))}
+        </div>
+    </div>
+    </div>
+)
+}
+
+
+
